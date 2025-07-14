5374607a7d83d1f725b5718f23fa5da77103fb7f
Minh Hieu Huynh <22520434@gm.uit.edu.vn>
2025-07-14 09:39:01 +0700

    Django Tutorial Part 5: Create our home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,26 @@
 from __future__ import annotations
 
-from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import (
+    Author,
+    Book,
+    BookInstance
+)
 
-# Create your views here.
+
+def index(request):
+    num_book = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    num_instances_available = BookInstance.objects.filter(
+        status__exact='a',
+    ).count()
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_book': num_book,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+    return render(request, 'index.html', context=context)
